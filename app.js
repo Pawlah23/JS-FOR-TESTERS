47d438186199ff1083ed2dcf230902909187ab6c
Pawlah <p.aghenta@gmail.com>
2024-02-12 18:04:10 +0100

    committimg files

---
+++ app.js
@@ -0,0 +1,89 @@
+// const myFavLanguage = 'JavaScript'
+
+// const age = 20
+// const name = 'Buchi'
+// const sentence = 'My name is ' + name +'. '+'I am '+age+' years old'
+
+
+// console.log(sentence)
+
+
+// const day = 'Thursday'
+// const position = 4
+
+// const sentence = 'Today is ' + day +'. '+'It is the '+position+'th day of the week'
+
+
+// console.log(sentence)
+
+// const age = 15
+
+// if(age>=18) {
+//     console.log('You are eligible to vote')
+// }  else {
+//     console.log('You are too young to vote')
+// }
+
+
+// const Number = 33
+
+// if(Number%2===0) {
+//     console.log('Number is even')
+// }  else {
+//     console.log('Number is Odd')
+// }
+
+const day = 'Saturday'
+
+
+// if(day==='Friday') {
+//     console.log('TGIF!')
+// } else if(day==='Saturday') {
+//     console.log('Yeh! The weekend is here!')
+// }else if(day==='Sunday') {
+//     console.log('Happy Sunday!')
+// }else {
+//     console.log('Go to work!')
+// }
+
+
+// switch(day){
+//     case 'Friday':
+//         console.log('TGIF')
+//         break
+//     case 'Saturday':
+//         console.log('Yeh! The weekend is here!')
+//         break
+//     case 'Sunday':
+//         console.log('Happy Sunday!')
+//         break
+//     default:
+//         console.log('Go to work!')    
+// }   
+
+// let star = 1 // starting point
+// while(star <=10) { 
+//     if(star===1) {
+//         console.log(star +' star')
+//     } else {
+//         console.log(star +' stars')
+//     }
+//     star = star + 1
+// }
+
+// for(let star = 0; star <=10; star = star+1) {
+//     if(star===1 || star===0) {
+//         console.log(star +' star')  
+//     } else {
+//         console.log(star +' stars')
+//     }
+        
+// }
+
+for(let number = 1; number <=10; number = number+1) {
+    if(number % 2 ===0) {
+        console.log(number +' star')  
+    } else {
+        console.log(number +' number')
+    }
+}
